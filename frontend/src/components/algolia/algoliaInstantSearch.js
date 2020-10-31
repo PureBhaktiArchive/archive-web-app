a8832ce84f7b4e13e9ab6fbdcc9ccacf30c23e5c
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2020-10-31 15:23:37 +0700

    Revert "add algolia index name to env.local"
    This reverts commit e54ae71110b55253794375609d39dddf758ad8a0.

--- frontend/src/components/algolia/algoliaInstantSearch.js
+++ frontend/src/components/algolia/algoliaInstantSearch.js
@@ -1,14 +1,15 @@
-import algoliasearch from 'algoliasearch/lite';
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import algoliasearch from 'algoliasearch/lite';
 import {
   Highlight,
   Hits,
   InstantSearch,
   Pagination,
-  SearchBox
+  SearchBox,
 } from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 import '../../styles/App.css';
+import { css } from 'styled-components/macro'; //eslint-disable-line
 
 const searchClient = algoliasearch(
   process.env.REACT_APP_ALGOLIA_APPLICATION_ID,
