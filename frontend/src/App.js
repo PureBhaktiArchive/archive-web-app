ad37c593e34b3175cc26d24eccb494d3cce5d860
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2020-11-21 20:42:23 +0700

    Replace React app with Vanilla JS app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,85 @@
-import Agolia from 'components/algolia/algoliaInstantSearch.js';
-import MiniCenteredFooter from 'components/footers/MiniCenteredFooter';
-import TwoColumnWithInput from 'components/hero/TwoColumnWithInput';
-import 'instantsearch.css/themes/algolia.css';
-import React, { Component } from 'react';
-import 'styles/App.css';
-import 'styles/tailwind.css';
-import 'tailwindcss/dist/base.css';
+/*!
+ * sri sri guru gaurangau jayatah
+ */
 
-class App extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <TwoColumnWithInput />
-        <Agolia />
-        <MiniCenteredFooter />
-      </React.Fragment>
-    );
-  }
-}
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import {
+  hits,
+  numericMenu,
+  pagination,
+  refinementList,
+  searchBox,
+} from 'instantsearch.js/es/widgets';
+import './app.css';
 
-export default App;
+const searchClient = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+);
+
+const search = instantsearch({
+  indexName: process.env.ALGOLIA_INDEX,
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: '#searchbox',
+  }),
+  refinementList({
+    container: '#location-list',
+    attribute: 'location',
+    sortBy: ['name:asc'],
+  }),
+  refinementList({
+    container: '#language-list',
+    attribute: 'languages',
+    sortBy: ['name:asc'],
+  }),
+  refinementList({
+    container: '#sound-quality-list',
+    attribute: 'soundQualityRating',
+    sortBy: ['name:asc'],
+  }),
+  refinementList({
+    container: '#category-list',
+    attribute: 'category',
+    sortBy: ['name:asc'],
+  }),
+  refinementList({
+    container: '#year-list',
+    attribute: 'year',
+    sortBy: ['name:asc'],
+  }),
+  numericMenu({
+    container: '#percentage-menu',
+    attribute: 'percentage',
+    items: [
+      { label: 'Any' },
+      { label: 'More than 50%', start: 0.5 },
+      { label: 'More than 90%', start: 0.9 },
+    ],
+  }),
+  numericMenu({
+    container: '#duration-menu',
+    attribute: 'duration',
+    items: [
+      { label: 'Any' },
+      { label: 'Less than 15 minutes', end: 15 },
+      { label: '15-30 minutes', start: 15, end: 30 },
+      { label: '30 minutes and longer', start: 30 },
+    ],
+  }),
+  hits({
+    container: '#hits',
+    templates: {
+      item: document.getElementById('item-template').textContent,
+    },
+  }),
+  pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();
