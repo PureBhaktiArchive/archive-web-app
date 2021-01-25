d1fe7f3516a666769bca472a9c71273b737a0e4f
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-01-25 20:46:32 +0530

    Increase timeout.

--- functions/src/convert.f.ts
+++ functions/src/convert.f.ts
@@ -12,7 +12,7 @@ import { StorageFileReference } from './StorageFileReference';
 if (!admin.apps.length) admin.initializeApp();
 
 export default functions
-  .runWith({ timeoutSeconds: 120 })
+  .runWith({ timeoutSeconds: 540 })
   .database.ref('/audio/entries/{id}/file')
   .onWrite(async (change, { params: { id } }) => {
     // Don't process deletions
