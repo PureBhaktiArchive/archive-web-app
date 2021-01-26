06be6d9b25ca70540023ce8542d6df75bfc87968
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-01-26 11:19:01 +0530

    Upgrade to 4GB function to utilize high CPU.

--- functions/src/convert.f.ts
+++ functions/src/convert.f.ts
@@ -12,7 +12,7 @@ import { StorageFileReference } from './StorageFileReference';
 if (!admin.apps.length) admin.initializeApp();
 
 export default functions
-  .runWith({ timeoutSeconds: 540 })
+  .runWith({ timeoutSeconds: 540, memory: '4GB' })
   .database.ref('/audio/entries/{id}/file')
   .onWrite(async (change, { params: { id } }) => {
     // Don't process deletions
