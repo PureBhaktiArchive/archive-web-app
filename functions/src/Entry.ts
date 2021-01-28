1d5527e1e1be6f994df14e7159130666042a06e0
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-01-29 00:18:44 +0700

    Index from database (#10)

---
+++ functions/src/Entry.ts
@@ -0,0 +1,13 @@
+/*!
+ * sri sri guru gaurangau jayatah
+ */
+
+import { ContentDetails } from './ContentDetails';
+import { StorageFileReference } from './StorageFileReference';
+
+export interface Entry {
+  contentDetails: ContentDetails;
+  file: StorageFileReference;
+  duration?: number;
+  obsolete?: boolean;
+}
