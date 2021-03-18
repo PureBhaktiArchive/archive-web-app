498e0975b33ff59b206b092bf902649c799a2ab2
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-03-18 16:29:54 +0530

    Expect dates to be in ISO format.

--- functions/spec/dates.spec.ts
+++ functions/spec/dates.spec.ts
@@ -3,20 +3,26 @@
  */
 
 import { DateTime } from 'luxon';
-import { parsePseudoISODate } from '../src/dates';
+import {
+  formatReducedPrecisionDateForHumans,
+  parseReducedPrecisionIsoDate,
+  ReducedPrecisionDate,
+} from '../src/dates';
 
-describe('Pseudo ISO date', () => {
+describe('ISO date', () => {
   it.each`
-    input         | iso             | date            | precision
-    ${'19960422'} | ${'1996-04-22'} | ${'1996-04-22'} | ${'day'}
-    ${'19961100'} | ${'1996-11'}    | ${'1996-11-01'} | ${'month'}
-    ${'19910000'} | ${'1991'}       | ${'1991-01-01'} | ${'year'}
+    input           | date            | precision
+    ${'19960422'}   | ${'1996-04-22'} | ${'day'}
+    ${'1996-04-22'} | ${'1996-04-22'} | ${'day'}
+    ${'199611'}     | ${'1996-11-01'} | ${'month'}
+    ${'1996-11'}    | ${'1996-11-01'} | ${'month'}
+    ${'1991'}       | ${'1991-01-01'} | ${'year'}
+    ${'1991'}       | ${'1991-01-01'} | ${'year'}
   `(
-    '"$input" should be parsed to $iso with $precision precision and $date base date',
-    ({ input, date, precision, iso }) => {
-      const result = parsePseudoISODate(input);
+    '"$input" should be parsed as $date with $precision precision',
+    ({ input, date, precision }) => {
+      const result = parseReducedPrecisionIsoDate(input);
       expect(result).not.toBeNull();
-      expect(result?.iso).toStrictEqual(iso);
       expect(result?.date.valueOf()).toEqual(
         DateTime.fromISO(date + 'T00:00:00.000Z').valueOf()
       );
@@ -26,13 +32,44 @@ describe('Pseudo ISO date', () => {
 
   it.each([
     'some', // Arbitrary string
-    '123412', // Not enough digits
+    '12345', // Not enough digits
+    '2016-W21-3', // Week notation is not accepted
+    '2016-200', // Ordinal dates are not accepted
+    '2016200', // Ordinal dates are not accepted
+    '2016-05-25T09', // Time is not accepted
+    '2016-05-25T09:24', // Time is not accepted
+    '2016-05-25T09:24:15', // Time is not accepted
+    '2016-05-25T09:24:15.123', // Time is not accepted
+    '2016-05-25T0924', // Time is not accepted
+    '2016-05-25T092415', // Time is not accepted
+    '2016-05-25T092415.123', // Time is not accepted
+    '2016-05-25T09:24:15,123', // Time is not accepted
     '19991301', // Non-existing month
     '19991131', // Non-existing day of November
     '19990229', // Not a leap year
-    '189912131', // Earlier than 1900
     '19910023', // Month not specified but day is
+    '2005-05-00', // Day is zero, should be absent
+    '2005-00-00', // Day and month are zeros, should be absent
+    '2006-7', // One-digit month
+    '2006-07-4', // One-digit day
   ])('"%s" should produce null on parsing', (input) => {
-    expect(parsePseudoISODate(input)).toBeNull();
+    expect(parseReducedPrecisionIsoDate(input)).toBeNull();
   });
+
+  it.each`
+    isoDate         | precision  | formatted
+    ${'1996-04-02'} | ${'day'}   | ${'April 2, 1996'}
+    ${'1996-04-22'} | ${'day'}   | ${'April 22, 1996'}
+    ${'1996-04-22'} | ${'month'} | ${'April 1996'}
+    ${'1996-04-22'} | ${'year'}  | ${'1996'}
+  `(
+    '"$isoDate" with $precision precision should be formatted as “$formatted”',
+    ({ isoDate, precision, formatted }) => {
+      const date: ReducedPrecisionDate = {
+        date: DateTime.fromISO(isoDate, { zone: 'utc', locale: 'en-US' }),
+        precision: precision,
+      };
+      expect(formatReducedPrecisionDateForHumans(date)).toEqual(formatted);
+    }
+  );
 });
