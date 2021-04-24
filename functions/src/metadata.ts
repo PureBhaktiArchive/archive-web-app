15a650ab73cfd5031228df4a234625bd0a51ef8d
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-04-24 20:03:52 +0530

    Combine `transcode` and `updateMetadata` functions

--- functions/src/metadata.ts
+++ functions/src/metadata.ts
@@ -34,20 +34,3 @@ export function composeMediaMetadata(
     date: contentDetails.date?.substr(0, 4),
   };
 }
-
-export const getFfmpegMetadataOptions = (
-  metadata: Record<string, string>
-): string[] =>
-  [
-    // Required because Windows only supports version up to 3 of ID3v2 tags
-    '-id3v2_version 3',
-    // the ID3v1 version to create legacy v1.1 tags
-    '-write_id3v1 1',
-    // Clearing all existing metadata, see https://gist.github.com/eyecatchup/0757b3d8b989fe433979db2ea7d95a01#3-cleardelete-id3-metadata
-    '-map_metadata -1',
-  ].concat(
-    Object.entries(metadata).flatMap(([name, value]) => [
-      '-metadata',
-      `${name}=${value || ''}`,
-    ])
-  );
