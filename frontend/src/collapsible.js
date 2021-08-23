788df1f1485850420cfe0080ea6f347165103c4c
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-08-23 21:09:18 +0530

    Use `x-show.transition` directive and avoid scaling

--- frontend/src/collapsible.js
+++ frontend/src/collapsible.js
@@ -12,9 +12,6 @@ window.collapsible = () => ({
     '@click': 'toggle',
   },
   contents: {
-    'x-show': 'expanded',
-    'x-transition:enter': 'transition ease-out duration-500',
-    'x-transition:enter-start': 'opacity-0 transform scale-90',
-    'x-transition:enter-end': 'opacity-100 transform scale-100',
+    'x-show.transition.opacity.duration.200ms': 'expanded',
   },
 });
