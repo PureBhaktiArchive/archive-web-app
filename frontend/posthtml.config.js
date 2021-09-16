959233c708553b8a0b06b7b58e5dd8c3fc9891d4
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2021-09-16 20:29:07 +0530

    Add `posthtml-extend` plugin

--- frontend/posthtml.config.js
+++ frontend/posthtml.config.js
@@ -4,6 +4,9 @@
 
 module.exports = {
   plugins: {
+    'posthtml-extend': {
+      'root': './src'
+    },
     // In order to use environment variables in HTML
     // Inspired by https://github.com/parcel-bundler/parcel/issues/1209#issuecomment-432424397
     'posthtml-expressions': {
