6f7f88dabb4b583edad31d088dffba78faedaaed
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2022-01-29 14:31:14 +0530

    Add seniority to memories index

--- functions/src/memories/index.f.ts
+++ functions/src/memories/index.f.ts
@@ -42,53 +42,59 @@ export default functions.pubsub
     const records = rows
       // Using flatMap to filter and map array in one function
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap#for_adding_and_removing_items_during_a_map
-      .flatMap<MemoriesAlgoliaRecord>((row) => {
-        // Only ready for launch are considered
-        if (!row['Ready for Launch']) return [];
+      .flatMap(
+        (row): MemoriesAlgoliaRecord | readonly MemoriesAlgoliaRecord[] => {
+          // Only ready for launch are considered
+          if (!row['Ready for Launch']) return [];
 
-        // Validation
-        const date = fromSerialDate(row['Date of memory (yyyy/mm/dd)'], 'utc');
+          // Validation
+          const date = fromSerialDate(
+            row['Date of memory (yyyy/mm/dd)'],
+            'utc'
+          );
 
-        if (!date.isValid) {
-          console.error(row['Sr No'], 'incorrect date');
-          return [];
+          if (!date.isValid) {
+            console.error(row['Sr No'], 'incorrect date');
+            return [];
+          }
+
+          if (!Number.isFinite(row['Duration (of the final edited video)'])) {
+            console.error(row['Sr No'], 'incorrect duration');
+            return [];
+          }
+
+          const videoId = row['YouTube link'].match(YouTubeIdRegex)?.[1];
+          if (!videoId) {
+            console.error(row['Sr No'], 'cannot extract YouTube ID');
+            return [];
+          }
+
+          return {
+            objectID: row['Sr No'].toString(),
+            programName: row['Program name'],
+            seniority: row.Seniority,
+            speakerName: row["Speaker's name"],
+            speakerCountry: row["Speaker's country"],
+            speakerIntro: row["Speaker's Intro"],
+            hostName: row.Host,
+            date: date.valueOf(),
+            dateForHumans: date
+              .setLocale('en-gb')
+              .toLocaleString(DateTime.DATE_FULL),
+            language: row.Language,
+            videoId,
+            // Converting duration from spreadsheet-based to seconds
+            duration: Math.ceil(
+              row['Duration (of the final edited video)'] * 24 * 3600
+            ),
+            topics: row.Topics,
+            gurus: row['Guru Initials'].split(',').map((i) => ({
+              abbreviation: i.trim(),
+              fullName: gurus.get(i.trim()) ?? null,
+            })),
+          };
         }
-
-        if (!Number.isFinite(row['Duration (of the final edited video)'])) {
-          console.error(row['Sr No'], 'incorrect duration');
-          return [];
-        }
-
-        const videoId = row['YouTube link'].match(YouTubeIdRegex)?.[1];
-        if (!videoId) {
-          console.error(row['Sr No'], 'cannot extract YouTube ID');
-          return [];
-        }
-
-        return {
-          objectID: row['Sr No'].toString(),
-          programName: row['Program name'],
-          speakerName: row["Speaker's name"],
-          speakerCountry: row["Speaker's country"],
-          speakerIntro: row["Speaker's Intro"],
-          hostName: row.Host,
-          date: date.valueOf(),
-          dateForHumans: date
-            .setLocale('en-gb')
-            .toLocaleString(DateTime.DATE_FULL),
-          language: row.Language,
-          videoId,
-          // Converting duration from spreadsheet-based to seconds
-          duration: Math.ceil(
-            row['Duration (of the final edited video)'] * 24 * 3600
-          ),
-          topics: row.Topics,
-          gurus: row['Guru Initials'].split(',').map((i) => ({
-            abbreviation: i.trim(),
-            fullName: gurus.get(i.trim()) ?? null,
-          })),
-        };
-      });
+      );
 
     const client = algoliasearch(
       functions.config().algolia.appid,
