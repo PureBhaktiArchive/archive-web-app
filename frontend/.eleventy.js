17051ded3ea0484200d1b009ebd958155fe9fe39
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2022-03-03 23:27:11 +0530

    Add basic config file with passthrough

---
+++ frontend/.eleventy.js
@@ -0,0 +1,23 @@
+/**
+ * Eleventy config function.
+ *
+ * Typing idea borrowed from https://github.com/11ty/eleventy/discussions/2089
+ *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
+ *  @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
+ */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('fonts');
+  eleventyConfig.addPassthroughCopy('images');
+  eleventyConfig.addPassthroughCopy('src/**/*.js');
+  eleventyConfig.addPassthroughCopy('src/**/*.css');
+  eleventyConfig.addPassthroughCopy('src/*.webmanifest');
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: 'src',
+      layouts: '_layouts',
+    },
+    templateFormats: [],
+  };
+};
