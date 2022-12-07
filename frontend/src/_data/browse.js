ab2315c20aedce23f94d8ed909cf08d50a1495f6
Joydev <joydev01@gmail.com>
2022-12-07 18:14:24 +0530

    writing and fetching json from root

--- frontend/src/_data/browse.js
+++ frontend/src/_data/browse.js
@@ -2,6 +2,7 @@
  * sri sri guru gaurangau jayatah
  */
 
+const filesystem = require('fs');
 const algoliasearch = require('algoliasearch');
 const { AssetCache } = require('@11ty/eleventy-fetch');
 
@@ -10,6 +11,24 @@ const client = algoliasearch(
   process.env.ALGOLIA_API_KEY
 );
 const index = client.initIndex(process.env.ALGOLIA_INDEX);
+
+let hits = [];
+index
+  .browseObjects({
+    batch: (batch) => {
+      hits = hits.concat(batch);
+    },
+  })
+  .then(() => {
+    const data = JSON.stringify(hits);
+    filesystem.writeFile('./../lectures.json', data, (err) => {
+      if (err) {
+        console.log('Error writing file', err);
+      } else {
+        console.log('JSON data is written to the file successfully');
+      }
+    });
+  });
 
 module.exports = async function () {
   // Pass in your unique custom cache key
@@ -20,16 +39,7 @@ module.exports = async function () {
     // return cached data.
     return asset.getCachedValue();
   }
-  let hits = [];
-  let algoliaHitsContent = index
-    .browseObjects({
-      batch: (batch) => {
-        hits = hits.concat(batch);
-      },
-    })
-    .then(() => {
-      return hits;
-    });
+  const algoliaHitsContent = require('../../../lectures.json');
   await asset.save(algoliaHitsContent, 'json');
   return algoliaHitsContent;
 };
