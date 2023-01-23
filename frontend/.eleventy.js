5a3dc14a01200396d86e13ae120be002c63baea9
Joydev <joydev01@gmail.com>
2023-01-23 13:52:39 +0530

    11ty filter added for duration

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -1,7 +1,9 @@
 /*!
  * sri sri guru gaurangau jayatah
  */
+
 const { EleventyRenderPlugin } = require('@11ty/eleventy');
+
 /**
  * Eleventy config function.
  *
@@ -13,6 +15,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('tailwind.config.js');
   eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'));
   eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+  //Filter for duration calculation
+  eleventyConfig.addFilter('duration', function (durationInSeconds) {
+    const formatDurationForHumans = new Date(1000 * durationInSeconds)
+      .toISOString()
+      .substring(Math.max(durationInSeconds || 0) >= 3600 ? 11 : 14, 19);
+    return formatDurationForHumans;
+  });
+
   // Return your Object options:
   return {
     dir: {
