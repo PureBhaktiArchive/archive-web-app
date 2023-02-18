4efd415dc426f299a81217e68923265f8001b3ea
Joydev <joydev01@gmail.com>
2023-02-18 17:49:10 +0530

    initialised Alpine for audio player

--- frontend/src/audio.js
+++ frontend/src/audio.js
@@ -4,27 +4,29 @@
 
 import Alpine from 'alpinejs';
 import './player';
-Alpine.start();
 
-let article = document.querySelector('#audio-page');
-/** type @{HTMLElement} */ // JSDoc types
-if (article instanceof HTMLElement) {
-  window.dispatchEvent(
-    new CustomEvent('archive:toggle-play', {
-      detail: {
-        fileId: article.dataset.audioNumber,
-        shouldPlay: false,
-        contentDetails: {
-          title: article.dataset.audioTitle,
-          dateForHumans: article.dataset.dateforhumans,
-          dateUncertain: article.dataset.dateuncertain,
-          location: article.dataset.location,
-          locationUncertain: article.dataset.locationuncertain,
-          category: article.dataset.category,
-          languages: JSON.parse(article.dataset.languages),
-          duration: article.dataset.duration,
+document.addEventListener('alpine:initialized', () => {
+  let article = document.querySelector('#audio-page');
+  /** type @{HTMLElement} */ // JSDoc types
+  if (article instanceof HTMLElement) {
+    window.dispatchEvent(
+      new CustomEvent('archive:toggle-play', {
+        detail: {
+          fileId: article.dataset.audioNumber,
+          shouldPlay: false,
+          contentDetails: {
+            title: article.dataset.audioTitle,
+            dateForHumans: article.dataset.dateforhumans,
+            dateUncertain: article.dataset.dateuncertain,
+            location: article.dataset.location,
+            locationUncertain: article.dataset.locationuncertain,
+            category: article.dataset.category,
+            languages: JSON.parse(article.dataset.languages),
+            duration: article.dataset.duration,
+          },
         },
-      },
-    })
-  );
-}
+      })
+    );
+  }
+});
+Alpine.start();
