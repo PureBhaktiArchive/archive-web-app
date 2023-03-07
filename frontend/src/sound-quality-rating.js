e553ca50b546f7dc2ee3b409fed9f8e58ef07d02
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-03-07 12:53:58 +0530

    Add comments on CJS exports syntax

--- frontend/src/sound-quality-rating.js
+++ frontend/src/sound-quality-rating.js
@@ -2,6 +2,8 @@
  * sri sri guru gaurangau jayatah
  */
 
+// Using CJS syntax because we have to import it in Eleventy config, which does not support ESM yet.
+// https://github.com/11ty/eleventy/issues/836
 module.exports.soundQualityRatingMapping = {
   Good: { label: 'Good', order: 1, color: 'bg-emerald-100' },
   Average: { label: 'Average', order: 2, color: 'bg-yellow-100' },
