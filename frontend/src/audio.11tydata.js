cb5a9086f3bfcd4e080c26ef507aabbfa93b307a
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-03-12 13:21:16 +0530

    Add `contentDetails` computed data

--- frontend/src/audio.11tydata.js
+++ frontend/src/audio.11tydata.js
@@ -6,4 +6,31 @@ const path = require('path');
 
 module.exports = {
   audios: require(path.resolve(__dirname, '../', process.env.AUDIOS_DATA_PATH)),
+  pagination: {
+    data: 'audios',
+    size: 1,
+    alias: 'audio',
+  },
+  eleventyComputed: {
+    // Compiling content details for the audio player
+    contentDetails: ({
+      audio: {
+        title,
+        dateForHumans,
+        dateUncertain,
+        location,
+        locationUncertain,
+        category,
+        languages,
+      },
+    }) => ({
+      title,
+      dateForHumans,
+      dateUncertain,
+      location,
+      locationUncertain,
+      category,
+      languages,
+    }),
+  },
 };
