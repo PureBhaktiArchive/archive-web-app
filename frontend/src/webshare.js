ce4edd98254b77b7326c08d76261948c15e3d6eb
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-03-20 13:40:29 +0530

    Pass url and title to the webshare component dynamically

--- frontend/src/webshare.js
+++ frontend/src/webshare.js
@@ -14,20 +14,31 @@ import tippy from 'tippy.js';
 Alpine.data(
   'webshare',
   /**
+   * This component can be statically initialized using the paramters.
+   * Alternatively it gets title and url dynamically from the `data` atributes.
+   * This allows for changing these values dynamically from outside the component, i.e. in the audio player.
    * @param {string} title
    * @param {string} url
    * @returns {import('alpinejs').AlpineComponent<WebshareComponent>}
    */
   (title, url) => ({
-    title: title,
-    url: new URL(url, window.location.href).toString(), // Making the url absolute
+    title,
+    url,
 
     async share() {
-      // Using Web Share API if it's supported, otherwise resorting to copying to clipboard and showing notification
+      const absoluteUrl = new URL(
+        this.url ?? this.$el.dataset.webshareUrl,
+        window.location.href
+      ).href;
+
+      // Using Web Share API if it's supported, otherwise resorting to copying to clipboard and showing a notification
       if (navigator.share)
-        await navigator.share({ title: this.title, url: this.url });
+        await navigator.share({
+          title: this.title ?? this.$el.dataset.webshareTitle,
+          url: absoluteUrl,
+        });
       else {
-        await navigator.clipboard.writeText(this.url);
+        await navigator.clipboard.writeText(absoluteUrl);
         const tippyElement =
           /** @type {import('tippy.js').ReferenceElement} */ (this.$el);
         // Creating a tippy instance if it's not created yet
