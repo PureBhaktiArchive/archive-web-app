6e17f223719d00de0103dcf0cbc848e52b60db33
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-03-20 11:58:13 +0530

    Rewrite webshare component
    Make it type-safe, and create tippy instance in case it is not created.

--- frontend/src/webshare.js
+++ frontend/src/webshare.js
@@ -3,25 +3,40 @@
  */
 
 import Alpine from 'alpinejs';
+import tippy from 'tippy.js';
+
+/**
+ * @typedef {Object} WebshareComponent
+ * @property {string} url
+ * @property {string} title
+ */
 
 Alpine.data(
   'webshare',
-  (/** @type {string} */ title, /** @type {string} */ pathName) => ({
-    async triggerEvent() {
-      let relUrl = typeof pathName !== 'undefined' ? pathName : '';
-      let url = new URL(relUrl, location.origin).toString();
-      if (navigator.share) {
-        navigator.share({ title, url });
-      } else {
-        await navigator.clipboard.writeText(url);
-        /** @type {import('tippy.js').ReferenceElement} */ (
-          this.$el
-        )._tippy?.show();
+  /**
+   * @param {string} title
+   * @param {string} url
+   * @returns {import('alpinejs').AlpineComponent<WebshareComponent>}
+   */
+  (title, url) => ({
+    title: title,
+    url: new URL(url, window.location.href).toString(), // Making the url absolute
+
+    async share() {
+      // Using Web Share API if it's supported, otherwise resorting to copying to clipboard and showing notification
+      if (navigator.share)
+        await navigator.share({ title: this.title, url: this.url });
+      else {
+        await navigator.clipboard.writeText(this.url);
+        const tippyElement =
+          /** @type {import('tippy.js').ReferenceElement} */ (this.$el);
+        // Creating a tippy instance if it's not created yet
+        (tippyElement._tippy ?? tippy(this.$el)).show();
       }
     },
 
     self: {
-      '@click': 'triggerEvent',
+      '@click': 'share',
     },
   })
 );
