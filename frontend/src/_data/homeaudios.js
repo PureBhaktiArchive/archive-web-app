4453e019a6089f0491888d2cc48b273bf978aede
Joydev <joydev01@gmail.com>
2023-04-13 10:20:10 +0530

    updating home page audios and other sections

---
+++ frontend/src/_data/homeaudios.js
@@ -0,0 +1,11 @@
+/*!
+ * sri sri guru gaurangau jayatah
+ */
+
+const path = require('path');
+
+module.exports = require(path.resolve(
+  __dirname,
+  '../../',
+  process.env.AUDIOS_DATA_PATH
+));
