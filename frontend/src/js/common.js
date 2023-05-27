1802caf4f8132aac05a1c04b8ded87a0357f1d34
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-05-27 12:48:09 +0530

    Defer Alpine start to allow for components registration

--- frontend/src/js/common.js
+++ frontend/src/js/common.js
@@ -6,7 +6,11 @@ import Alpine from 'alpinejs';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
-Alpine.start();
+// Defer `Alpine.start` to allow other code register the Alpine extensions
+// Approach taken from https://github.com/alpinejs/alpine/discussions/1705#discussioncomment-976821
+queueMicrotask(() => {
+  Alpine.start();
+});
 
 // Only in development according to https://vitejs.dev/guide/env-and-mode.html#env-variables
 if (import.meta.env.DEV) {
