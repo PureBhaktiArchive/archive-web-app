f7b9cbb83eaaf821b687517d9b42832706f0f6b3
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-12-28 09:48:40 +0700

    Remove `publish`

--- functions/src/index.js
+++ functions/src/index.js
@@ -3,13 +3,11 @@
  */
 
 import { default as importAudioRecords } from './audios/import.js';
-import { default as publishAudioRecords } from './audios/publish.js';
 import { default as updateFile } from './audios/update-file.js';
 import { default as updateMemoriesIndex } from './memories/update-search-index.js';
 
 export const audios = {
   import: importAudioRecords,
-  publish: publishAudioRecords,
   file: {
     update: updateFile,
   },
