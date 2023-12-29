3baffa9b2b9aae6f6673c4945b19b751e50accb2
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-12-29 20:18:11 +0700

    Remove the `audios.importRecords` cloud function

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,12 +2,10 @@
  * sri sri guru gaurangau jayatah
  */
 
-import { default as importAudioRecords } from './audios/import.js';
 import { default as updateFile } from './audios/update-file.js';
 import { default as updateMemoriesIndex } from './memories/update-search-index.js';
 
 export const audios = {
-  import: importAudioRecords,
   file: {
     update: updateFile,
   },
