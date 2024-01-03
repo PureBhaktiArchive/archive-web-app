d65124f18cb66321d37e17b324b130cc2de8b742
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-01-04 00:45:56 +0700

    Remove the `update-file` function

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,14 +2,7 @@
  * sri sri guru gaurangau jayatah
  */
 
-import { default as updateFile } from './audios/update-file.js';
 import { default as updateMemoriesIndex } from './memories/update-search-index.js';
-
-export const audios = {
-  file: {
-    update: updateFile,
-  },
-};
 
 export const memories = {
   index: updateMemoriesIndex,
