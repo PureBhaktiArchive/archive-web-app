eefe96ef6daa67f9fa237e05a5abdf3f9bd9cb77
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-01-14 19:06:17 +0700

    Remove functions entirely
    TADA!

--- functions/src/index.js
+++
@@ -1,9 +0,0 @@
-/*!
- * sri sri guru gaurangau jayatah
- */
-
-import { default as updateMemoriesIndex } from './memories/update-search-index.js';
-
-export const memories = {
-  index: updateMemoriesIndex,
-};
