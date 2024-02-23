d360ce9355cc12b6f36bcd794566fc0a6dfe42c9
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-02-23 11:55:40 +0700

    Document using aliases

--- frontend/src/_data/audios.js
+++ frontend/src/_data/audios.js
@@ -15,6 +15,7 @@ module.exports = ({ directus }) =>
       // Fetching all items. Later, when number of records grows, we may need to fetch in pages as described in https://docs.directus.io/reference/query.html#offset
       // Also, we assume that the `QUERY_LIMIT_MAX` env variable is not set on the server.
       limit: -1,
+      // Historically, in the code we refer to the `id` as `fileId`. Therefore using aliases here.
       alias: { fileId: 'id' },
     })
   );
