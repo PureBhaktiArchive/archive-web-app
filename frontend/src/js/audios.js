8ab17b87b86f31effff5dfa84c0f74a321a77e53
Joydev <joydev01@gmail.com>
2024-02-28 19:16:03 +0530

    series listing is removed from filter panel

--- frontend/src/js/audios.js
+++ frontend/src/js/audios.js
@@ -131,11 +131,6 @@ search.addWidgets([
     limit: 100,
     sortBy: ['name:asc'],
   }),
-  refinementList({
-    container: '#series-list div:empty',
-    attribute: 'series',
-    sortBy: ['name:asc'],
-  }),
   numericMenu({
     container: '#percentage-menu div:empty',
     attribute: 'percentage',
