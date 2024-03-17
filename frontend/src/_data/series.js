98642cd0c8fd04b367453bff60c7484793268147
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-03-17 17:12:02 +0700

    Transform the series data structure after fetching

--- frontend/src/_data/series.js
+++ frontend/src/_data/series.js
@@ -7,13 +7,63 @@ const { readItems } = require('@directus/sdk');
 /**
  * @param {EleventyGlobalData} data
  */
-
 module.exports = ({ directus }) =>
-  directus.request(
-    readItems('series', {
-      fields: ['*.*.*'],
-      filter: { status: { _eq: 'published' } },
-      // Also, we assume that the `QUERY_LIMIT_MAX` env variable is not set on the server.
-      limit: -1,
-    })
-  );
+  directus
+    .request(
+      readItems('series', {
+        fields: [
+          '*',
+          {
+            audios: [
+              {
+                audios_id: [
+                  'category',
+                  'date',
+                  'dateUncertain',
+                  'duration',
+                  'id',
+                  'fileId',
+                  'languages',
+                  'location',
+                  'locationUncertain',
+                  'otherSpeakers',
+                  'percentage',
+                  'soundQualityRating',
+                  'status',
+                  'timeOfDay',
+                  'title',
+                ],
+              },
+            ],
+          },
+        ],
+        filter: { status: { _eq: 'published' } },
+        sort: 'sort',
+        deep: {
+          audios: {
+            _sort: 'sort',
+            // At this time, alias has not been typed yet for use in other query parameters like deep. See https://docs.directus.io/guides/sdk/types.html#working-with-input-query-types
+            _alias: {
+              // Historically, in the code we refer to the `id` as `fileId`. Therefore using aliases here.
+              fileId: 'id',
+            },
+          },
+        },
+        limit: -1,
+      })
+    )
+    // Transforming the response to fit our expected data structure
+    .then((response) =>
+      response.map(({ id, title, audios }) => ({
+        id,
+        title,
+        // Eliminating the junction object manually because it is not yet possible to skip it in the query: https://github.com/directus/directus/discussions/4112
+        audios: audios.map(
+          /**
+           * We need to specify the type explicitly because Directus' type is not recognized properly
+           * @param {{ audios_id: Omit<Audio, 'topics'>; }} serieAudio
+           */
+          (serieAudio) => serieAudio.audios_id
+        ),
+      }))
+    );
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -3,8 +3,12 @@ import { RestClient } from '@directus/sdk';
 /**
  * Declaring types here to access types without importing in JS files according to
  * https://docs.joshuatz.com/cheatsheets/js/jsdoc/#passing-types-around-via-typescript-files
+ *
  * Wrapping with `declare global` because we have imports above, according to
  * https://docs.joshuatz.com/cheatsheets/js/jsdoc/#vscode-type-safety-jsdoc---issues
+ *
+ * Using a plain *.ts file instead of an ambient declaration file in order to enforce type checks here.
+ * There is no check in *.d.ts if `skipLibCheck` is `true`. See https://stackoverflow.com/q/71700154/3082178
  */
 
 declare global {
@@ -20,7 +24,7 @@ declare global {
     short_video: Video;
   };
 
-  type Audio = {
+  interface Audio {
     id: number;
     fileId: number; // Including this manually because Directus does not include query aliases in the field types. See https://github.com/directus/directus/blob/c45cd69dd723e0a536cab537aff117d5861868e7/sdk/src/types/query.ts#L69-L73
     status: 'active' | 'inactive';
@@ -37,14 +41,7 @@ declare global {
     languages: string[];
     otherSpeakers?: string[];
     duration: number;
-  };
-
-  type Series = {
-    id: number;
-    status: string;
-    title: string;
-    audios: number;
-  };
+  }
 
   type AudioForSearch = Pick<
     Audio,
@@ -69,19 +66,36 @@ declare global {
     languageCategory: string;
   };
 
-  type DirectusSchema = {
+  interface Serie {
+    id: number;
+    status: 'published' | 'draft' | 'archived';
+    title: string;
+    sort: number;
+    audios: SerieAudio[];
+  }
+
+  interface SerieAudio {
+    id: number;
+    series_id: number;
+    audios_id: Audio;
+    sort: number;
+  }
+
+  /**
+   * @see https://docs.directus.io/guides/sdk/types.html
+   */
+  interface DirectusSchema {
     home: Home;
     videos: Video[];
     audios: Audio[];
     series: Serie[];
-  };
-
-  type DirectusClient = RestClient<DirectusSchema>;
+    series_audios: SerieAudio[];
+  }
 
   type EleventyGlobalData = {
     home: Home;
     audios: Audio[];
     series: Serie[];
-    directus: DirectusClient;
+    directus: RestClient<DirectusSchema>;
   };
 }
