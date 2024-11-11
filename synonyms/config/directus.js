bd78a391c0f17e0a3d3947f1c391675570404d10
Joydev <joydev01@gmail.com>
2024-11-11 16:30:09 +0530

    unnecessary files removed

--- synonyms/config/directus.js
+++
@@ -1,7 +0,0 @@
-import { createDirectus, rest, staticToken } from "@directus/sdk";
-
-const client = createDirectus(process.env.DIRECTUS_URL)
-  .with(staticToken(process.env.DIRECTUS_STATIC_TOKEN))
-  .with(rest());
-
-export { client };
